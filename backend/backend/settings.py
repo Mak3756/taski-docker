43f69a94e1c8ddac2a32ef591a9abd790f814837
Maksim <maksim3758@gmail.com>
2026-01-28 00:06:51 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,7 +95,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/backend_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
